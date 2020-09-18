672436bff5635430f84c336e61ccde9df4c7c8c9
figsoda <figsoda@pm.me>
2020-09-17 22:23:35 -0400

    Add scripts to install packages

---
+++ install-pkgs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+xbps-install -Syu
+xbps-install -y void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
+
+while IFS= read -r pkgs do
+    [ -n "$pkgs" ] && xbps-install -y "$pkgs"
+done < xbps-pkgs.txt
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install com.discordapp.Discord
+
+eval "$(curl -L https://nixos.org/nix/install)" --no-daemon
+eval "$(curl -L https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
