32971145020976071ed07bff1c1d759f3c4437b3
figsoda <figsoda@pm.me>
2020-09-18 12:58:32 -0400

    Install cargo packages

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -7,6 +7,9 @@ while IFS= read -r pkgs; do
     [ -n "$pkgs" ] && xbps-install -y "$pkgs"
 done < xbps-pkgs.txt
 
+rustup toolchain install nightly --components clippy rustfmt
+cargo install cargo-audit cargo-bloat cargo-cache cargo-udeps cargo-update cross pactorio
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak override --filesystem=~/.icons/:ro --user
 flatpak install com.discordapp.Discord
