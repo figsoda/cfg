4ff7788982f9ddc1c09f443eafdf5f933328533d
figsoda <figsoda@pm.me>
2020-09-19 15:33:39 -0400

    Correctly install rust toolchain

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -10,7 +10,7 @@ done < xbps-pkgs.txt
 ln -st /var/service /etc/sv/{NetworkManager,bluetoothd,chronyd,dbus,docker,sddm}
 touch /etc/sv/docker/down
 
-rustup toolchain install nightly --components clippy rustfmt
+rustup-init -yt nightly -c clippy rustfmt
 cargo install cargo-audit cargo-bloat cargo-cache cargo-udeps cargo-update cross pactorio
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
