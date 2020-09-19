42b25979849ab6d894caa1746366e75ea8326811
figsoda <figsoda@pm.me>
2020-09-19 14:09:34 -0400

    Remove quotes to split into multiple args

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -4,7 +4,7 @@ xbps-install -Suy
 xbps-install -y void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 
 while IFS= read -r pkgs; do
-    [ -n "$pkgs" ] && xbps-install -y "$pkgs"
+    [ -n "$pkgs" ] && xbps-install -y $pkgs
 done < xbps-pkgs.txt
 
 ln -st /var/service /etc/sv/{NetworkManager,bluetoothd,chronyd,dbus,docker,sddm}
