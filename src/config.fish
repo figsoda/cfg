94a252ea5f4e61c31931eb38828fa7be66d950a3
figsoda <figsoda@pm.me>
2020-09-24 12:16:32 -0400

    Add redo alias

--- src/config.fish
+++ src/config.fish
@@ -30,6 +30,7 @@ set -gx EDITOR code-oss
 set -gx VERSION_ID unavailable
 
 alias ls "exa -bl --git --time-style long-iso --group-directories-first"
+alias redo "eval sudo \$history[1]"
 alias code code-oss
 
 bash ~/.nix-profile/etc/profile.d/nix.sh
