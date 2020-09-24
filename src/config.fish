986fe468e2bde8965da5b6e632c49ffdd644e78e
figsoda <figsoda@pm.me>
2020-09-24 11:49:59 -0400

    Remove equal sign

--- src/config.fish
+++ src/config.fish
@@ -29,8 +29,8 @@ set -g fish_color_match normal
 set -gx EDITOR code-oss
 set -gx VERSION_ID unavailable
 
-alias ls="exa -bl --git --time-style long-iso --group-directories-first"
-alias code=code-oss
+alias ls "exa -bl --git --time-style long-iso --group-directories-first"
+alias code code-oss
 
 bash ~/.nix-profile/etc/profile.d/nix.sh
 thefuck --alias | source
