c4381ff2b2d30df79e6e48aa725d8334588aec5b
figsoda <figsoda@pm.me>
2020-10-07 13:46:25 -0400

    add more output messages

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+echo Installing void packages
 xbps-install -Suy void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 xbps-install -Suy $(< xbps-pkgs.txt)
 
+echo Creating runit service symlinks
 ln -st /var/service /etc/sv/{NetworkManager,bluetoothd,chronyd,dbus,docker,elogind}
 touch /etc/sv/docker/down
 
+echo Installing rust toolchain
 rustup-init -y --default-toolchain nightly -c clippy rustfmt
+
+echo Installing cargo packages
 ~/.cargo/bin/cargo install cargo-audit cargo-bloat cargo-cache cargo-udeps cargo-update cross pactorio
 
+echo Downloading JetBrains Mono Nerd Font
 for f in Bold-Italic Bold ExtraBold-Italic ExtraBold Italic Medium-Italic Medium Regular; do
     curl -LSso "$HOME/.local/share/fonts/JetBrains Mono ${f/-/ } Nerd Font Complete.ttf"\
         "https://github.com/ryanoasis/nerd-fonts/raw/2.1.0/patched-fonts/JetBrainsMono/$f/complete/JetBrains%20Mono%20${f/-/%20}%20Nerd%20Font%20Complete.ttf"
 done
 
+echo Downloading Arimo Nerd Font
 for f in Bold-Italic Bold Italic Regular; do
     curl -LSso "$HOME/.local/share/fonts/Arimo ${f/-/ } Nerd Font Complete.ttf"\
         "https://github.com/ryanoasis/nerd-fonts/raw/2.1.0/patched-fonts/Arimo/$f/complete/Arimo%20${f/-/%20}%20Nerd%20Font%20Complete.ttf"
@@ -46,10 +53,11 @@ mkdir -p ~/.themes
 unzip -qq "$tmp" -d ~/.themes
 unlink "$tmp"
 
+echo Downloading wallpaper
 curl -LSs https://github.com/dracula/wallpaper/raw/master/void.png -o ~/wallpaper.png
 
+echo Creating symlinks
 src="$(realpath "$(dirname "${BASH_SOURCE[0]}")")/src"
-
 while IFS=" " read -r file dir; do
     dir="$(eval echo "$dir")"
     if [ -n "$file" ] && [ -n "$dir" ]; then
@@ -59,4 +67,5 @@ while IFS=" " read -r file dir; do
     fi
 done < symlinks.txt
 
+echo Caching font information
 fc-cache
