ca455336efaa22bbd875097e3226d273abbc46a8
figsoda <figsoda@pm.me>
2020-11-29 19:22:35 -0500

    set useOSProber to true for grub bootloader

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,9 @@ in {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
-      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      grub.useOSProber = true;
+      systemd-boot.enable = true;
     };
   };
 
