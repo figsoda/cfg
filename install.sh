66c820257b2266c6a31ccfcd5ab9e9c5da1d5973
figsoda <figsoda@pm.me>
2020-11-30 15:54:39 -0500

    correctly remove symlink target

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ src="$(realpath "$(dirname "${BASH_SOURCE[0]}")")/src"
 while IFS=" " read -r file d; do
     dir="$(eval echo "$d")"
     if [ -n "$file" ] && [ -n "$dir" ]; then
-        [ -f "$dir/$file" ] && rm "$dir/$file"
+        [ -e "$dir/$file" ] && rm -r "$dir/$file"
         [ -d "$dir" ] || mkdir -p "$dir"
         ln -s "$src/$file" -t "$dir"
     fi
