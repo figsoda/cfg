ec3d4baa7180630b9cc3c485aaf0098461f48d7f
figsoda <figsoda@pm.me>
2021-01-19 12:58:15 -0500

    make cp and mv prompt before overwriting

--- lib/fish.nix
+++ lib/fish.nix
@@ -108,9 +108,10 @@
       end
     '';
     shellAliases = {
-      cp = "${coreutils}/bin/cp -r";
+      cp = "${coreutils}/bin/cp -ir";
       ls =
         "${exa}/bin/exa -bl --git --icons --time-style long-iso --group-directories-first";
+      mv = "${coreutils}/bin/mv -i";
       rm = "${coreutils}/bin/rm -I";
     };
   };
