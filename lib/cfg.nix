05960bc8fb365a19b2d698b4d43bfa0a7ae74d4d
figsoda <figsoda@pm.me>
2021-06-30 22:04:16 -0400

    fontconfig: source han sans -> sarasa gothic

--- lib/cfg.nix
+++ lib/cfg.nix
@@ -20,9 +20,9 @@
       defaultFonts = {
         emoji = [ "Noto Color Emoji" ];
         monospace =
-          [ "JetBrainsMono Nerd Font" "Cascadia Code" "Source Han Sans K" ];
-        sansSerif = [ "Arimo Nerd Font" "Source Han Sans K" ];
-        serif = [ "Arimo Nerd Font" "Source Han Sans K" ];
+          [ "JetBrainsMono Nerd Font" "Cascadia Code" "Sarasa Mono SC" ];
+        sansSerif = [ "Arimo Nerd Font" "Sarasa Gothic SC" ];
+        serif = [ "Arimo Nerd Font" "Sarasa Gothic SC" ];
       };
       includeUserConf = false;
     };
@@ -30,7 +30,7 @@
       cascadia-code
       (nerdfonts.override { fonts = [ "Arimo" "JetBrainsMono" ]; })
       noto-fonts-emoji
-      source-han-sans
+      sarasa-gothic
     ];
   };
 
