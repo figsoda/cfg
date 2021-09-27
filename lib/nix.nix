4f0c0185e95732fab066045ef7f421a93ec89f4a
figsoda <figsoda@pm.me>
2021-09-27 08:20:12 -0400

    minor styling changes

--- lib/nix.nix
+++ lib/nix.nix
@@ -9,15 +9,20 @@ let inherit (config.passthru) inputs; in
       "figsoda.cachix.org-1:mJfTEL4qLCqymqynJlaTxxi5APlaM0DfWg+h+CRGa20="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    binaryCaches =
-      [ "https://figsoda.cachix.org" "https://nix-community.cachix.org" ];
+    binaryCaches = [
+      "https://figsoda.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
     extraOptions = "experimental-features = flakes nix-command";
     gc = {
       automatic = true;
       dates = "Sat, 00:30";
       options = "--delete-older-than 3d";
     };
-    nixPath = [ "nixos=${inputs.nixpkgs}" "nixpkgs=${inputs.nixpkgs}" ];
+    nixPath = [
+      "nixos=${inputs.nixpkgs}"
+      "nixpkgs=${inputs.nixpkgs}"
+    ];
     package = pkgs.nixUnstable;
     registry.nixpkgs.flake = inputs.nixpkgs;
   };
