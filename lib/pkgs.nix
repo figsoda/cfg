04b6caaadacef8afccbf279aaf517d3d158f1d0c
figsoda <figsoda@pm.me>
2021-10-24 14:38:43 -0400

    r: simplify running cargo subcommands

--- lib/pkgs.nix
+++ lib/pkgs.nix
@@ -8,7 +8,12 @@ rec {
       ${libressl}/bin/openssl aes-256-cbc -d -in ~/.config/secrets/github
     '')
     (writeShellScriptBin "r" ''
-      ${config.nix.package}/bin/nix run "nixpkgs#$1" -- "''${@:2}"
+      if [ "$1" = cargo ] && nix eval --raw "nixpkgs#$1-$2" 2> /dev/null; then
+        pkg=$1-$2
+      else
+        pkg=$1
+      fi
+      ${config.nix.package}/bin/nix run "nixpkgs#$pkg" -- "''${@:2}"
     '')
     (writeShellScriptBin "rofi-todo" ''
       ${rofi}/bin/rofi -show todo -modi todo:${
