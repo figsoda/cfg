d7f11782322851f8e4f3882cb226f638f27387ef
figsoda <figsoda@pm.me>
2022-02-13 10:25:27 -0500

    update nix settings

--- lib/nix.nix
+++ lib/nix.nix
@@ -4,16 +4,6 @@ let inherit (config.passthru) inputs; in
 
 {
   nix = {
-    autoOptimiseStore = true;
-    binaryCachePublicKeys = [
-      "figsoda.cachix.org-1:mJfTEL4qLCqymqynJlaTxxi5APlaM0DfWg+h+CRGa20="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    binaryCaches = [
-      "https://figsoda.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    extraOptions = "experimental-features = flakes nix-command";
     gc = {
       automatic = true;
       dates = "Sat, 04:30";
@@ -25,6 +15,18 @@ let inherit (config.passthru) inputs; in
     ];
     package = pkgs.nixUnstable;
     registry.nixpkgs.flake = inputs.nixpkgs;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "flakes" "nix-command" ];
+      substituters = [
+        "https://figsoda.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "figsoda.cachix.org-1:mJfTEL4qLCqymqynJlaTxxi5APlaM0DfWg+h+CRGa20="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   nixpkgs = {
