4800b5abbec63bf0f751f518e20e8e8070cf99b9
figsoda <figsoda@pm.me>
2022-08-08 11:27:24 +0800

    nvim: fix luasnip setup

--- lib/nvim/snippets.lua
+++ lib/nvim/snippets.lua
@@ -36,113 +36,111 @@ local function fetch_sha256(args)
   return vim.v.shell_error == 0 and res[1] or ""
 end
 
-luasnip.snippets = {
-  nix = {
-    s("buildRustPackage", {
-      t({
-        "{ lib, rustPlatform, fetchFromGitHub }:",
-        "",
-        "rustPlatform.buildRustPackage rec {",
-        '  pname = "',
-      }),
-      i(1),
-      t({ '";', '  version = "' }),
-      i(2),
-      t({ '";', "", "  src = fetchFromGitHub {", '    owner = "' }),
-      i(3),
-      t({
-        '";',
-        "    repo = pname;",
-        "    rev = ",
-      }),
-      c(4, { t('"v${version}"'), t("version") }),
-      t({ ";", '    sha256 = "' }),
-      f(fetch_sha256, { 1, 2, 3, 4 }),
-      t({
-        '";',
-        "  };",
-        "",
-        '  cargoSha256 = "";',
-        "",
-        "  meta = with lib; {",
-        '    description = "";',
-        '    homepage = "https://github.com/',
-      }),
-      r(3),
-      t("/"),
-      r(1),
-      t({ '";', "    license = " }),
-      i(5),
-      t({
-        ";",
-        "    maintainers = with maintainers; [ figsoda ];",
-        "  };",
-        "}",
-      }),
-    }, {
-      condition = function()
-        return vim.fn.line(".") == 1
-      end,
+luasnip.add_snippets("nix", {
+  s("buildRustPackage", {
+    t({
+      "{ lib, rustPlatform, fetchFromGitHub }:",
+      "",
+      "rustPlatform.buildRustPackage rec {",
+      '  pname = "',
     }),
+    i(1),
+    t({ '";', '  version = "' }),
+    i(2),
+    t({ '";', "", "  src = fetchFromGitHub {", '    owner = "' }),
+    i(3),
+    t({
+      '";',
+      "    repo = pname;",
+      "    rev = ",
+    }),
+    c(4, { t('"v${version}"'), t("version") }),
+    t({ ";", '    sha256 = "' }),
+    f(fetch_sha256, { 1, 2, 3, 4 }),
+    t({
+      '";',
+      "  };",
+      "",
+      '  cargoSha256 = "";',
+      "",
+      "  meta = with lib; {",
+      '    description = "";',
+      '    homepage = "https://github.com/',
+    }),
+    r(3),
+    t("/"),
+    r(1),
+    t({ '";', "    license = " }),
+    i(5),
+    t({
+      ";",
+      "    maintainers = with maintainers; [ figsoda ];",
+      "  };",
+      "}",
+    }),
+  }, {
+    show_condition = function()
+      return vim.fn.line(".") == 1
+    end,
+  }),
 
-    s("flake", {
-      t({
-        "{",
-        "  inputs = {",
-        '    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";',
-        "  };",
-        "",
-        "  outputs = { self, nixpkgs }: {",
-        "    ",
-      }),
-      i(0),
-      t({ "", "  };", "}" }),
-    }, {
-      condition = function()
-        return vim.fn.expand("%:t") == "flake.nix" and vim.fn.line(".") == 1
-      end,
+  s("flake", {
+    t({
+      "{",
+      "  inputs = {",
+      '    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";',
+      "  };",
+      "",
+      "  outputs = { self, nixpkgs }: {",
+      "    ",
     }),
+    i(0),
+    t({ "", "  };", "}" }),
+  }, {
+    show_condition = function()
+      return vim.fn.expand("%:t") == "flake.nix" and vim.fn.line(".") == 1
+    end,
+  }),
 
-    s("mkDerivation", {
-      t({
-        "{ lib, stdenv, fetchFromGitHub }:",
-        "",
-        "stdenv.mkDerivation rec {",
-        '  pname = "',
-      }),
-      i(1),
-      t({ '";', '  version = "' }),
-      i(2),
-      t({ '";', "", "  src = fetchFromGitHub {", '    owner = "' }),
-      i(3),
-      t({ '";', "    repo = pname;", "    rev = " }),
-      c(4, { t('"v${version}"'), t("version") }),
-      t({ ";", '    sha256 = "' }),
-      f(fetch_sha256, { 1, 2, 3, 4 }),
-      t({
-        '";',
-        "  };",
-        "",
-        "  meta = with lib; {",
-        '    description = "";',
-        '    homepage = "https://github.com/',
-      }),
-      r(3),
-      t("/"),
-      r(1),
-      t({ '";', "    license = " }),
-      i(5),
-      t({
-        ";",
-        "    platforms = platforms.all;",
-        "    maintainers = with maintainers; [ figsoda ];",
-        "  };",
-        "}",
-      }),
-    }, {
-      condition = function()
-        return vim.fn.line(".") == 1
-      end,
+  s("mkDerivation", {
+    t({
+      "{ lib, stdenv, fetchFromGitHub }:",
+      "",
+      "stdenv.mkDerivation rec {",
+      '  pname = "',
     }),
-  },
-}
+    i(1),
+    t({ '";', '  version = "' }),
+    i(2),
+    t({ '";', "", "  src = fetchFromGitHub {", '    owner = "' }),
+    i(3),
+    t({ '";', "    repo = pname;", "    rev = " }),
+    c(4, { t('"v${version}"'), t("version") }),
+    t({ ";", '    sha256 = "' }),
+    f(fetch_sha256, { 1, 2, 3, 4 }),
+    t({
+      '";',
+      "  };",
+      "",
+      "  meta = with lib; {",
+      '    description = "";',
+      '    homepage = "https://github.com/',
+    }),
+    r(3),
+    t("/"),
+    r(1),
+    t({ '";', "    license = " }),
+    i(5),
+    t({
+      ";",
+      "    platforms = platforms.all;",
+      "    maintainers = with maintainers; [ figsoda ];",
+      "  };",
+      "}",
+    }),
+  }, {
+    show_condition = function()
+      return vim.fn.line(".") == 1
+    end,
+  }),
+})
