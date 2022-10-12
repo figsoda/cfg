9edba29cc5c673d7bf00fc2c71dcb657d3c88ed6
figsoda <figsoda@pm.me>
2022-10-12 13:18:06 -0400

    nvim: modernize autopairs

--- lib/nvim/autopairs.lua
+++ lib/nvim/autopairs.lua
@@ -1,9 +1,7 @@
-local highlighter = require("vim.treesitter.highlighter")
-local ts_utils = require("nvim-treesitter.ts_utils")
-
 local api = vim.api
 local fn = vim.fn
 local map = vim.keymap.set
+local treesitter = vim.treesitter
 
 local function get(s, i)
   return s:sub(i, i)
@@ -14,57 +12,41 @@ local function indent_pair(r)
   return string.format("<cr> <c-u><cr> <c-u>%s%s<up>%s<tab>", indent, r, indent)
 end
 
-local function is_string(y, x)
-  local buf = api.nvim_get_current_buf()
-  local hl = highlighter.active[buf]
-
-  if hl then
-    local tree = hl.tree
-    local lang = tree:lang()
-    local match = false
-
-    tree:for_each_tree(function(child)
-      if not child then
-        return
-      end
-
-      local root = child:root()
-      local start_row, _, end_row, _ = root:range()
-
-      if start_row > y - 1 or end_row < y - 1 then
-        return
-      end
-
-      local q = hl:get_query(lang)
-      local query = q:query()
-
-      if not query then
-        return
-      end
-
-      for capture, node in query:iter_captures(root, buf, y - 1, y) do
-        if
-          q.hl_cache[capture] and ts_utils.is_in_node_range(node, y - 1, x - 1)
-        then
-          local c = q._query.captures[capture]
-          if c and c:match("^string") then
-            match = true
-            return
-          end
-        end
-      end
-    end)
-
-    return match
+local function ts_string(buf, x, y)
+  for _, capture in pairs(treesitter.get_captures_at_pos(buf, y - 1, x)) do
+    if capture.capture:match("^string") then
+      return true
+    end
   end
+  return false
+end
 
-  local ids = fn.synstack(y, x)
+local function syn_string(x, y)
+  local ids = fn.synstack(y, x + 1)
   local len = #ids
   if len == 0 then
     return false
   else
     local syn = fn.synIDattr(ids[len], "name")
-    return syn:match("String") or syn:match("InterpolationDelimiter")
+    return syn:find("String") or syn:find("InterpolationDelimiter")
+  end
+end
+
+local function in_string(x, y)
+  local last_x, last_y
+  if x == 0 then
+    last_y = y - 1
+    last_x = math.max(#fn.getline(last_y), 1)
+  else
+    last_x = x - 1
+    last_y = y
+  end
+
+  local buf = api.nvim_get_current_buf()
+  if treesitter.highlighter.active[buf] then
+    return ts_string(buf, x, y) and ts_string(buf, last_x, last_y)
+  else
+    return syn_string(x, y) and syn_string(last_x, last_y)
   end
 end
 
@@ -92,7 +74,7 @@ end, { expr = true })
 
 api.nvim_create_autocmd("FileType", {
   pattern = "nix",
-  callback = function()
+  callback = function(ctx)
     map("i", "<cr>", function()
       local line = api.nvim_get_current_line()
       local x = fn.col(".")
@@ -104,23 +86,16 @@ api.nvim_create_autocmd("FileType", {
       else
         return "<cr>"
       end
-    end, { buffer = true, expr = true })
+    end, { buffer = ctx.buf, expr = true })
   end,
 })
 
 for l, r in pairs(autopairs) do
   if l == r then
     map("i", l, function()
-      local x = fn.col(".")
-      local y = fn.line(".")
-      if
-        is_string(y, x)
-        and (
-          x == 1 and is_string(y - 1, math.max(#fn.getline(y - 1), 1))
-          or is_string(y, x - 1)
-        )
-      then
-        return get(api.nvim_get_current_line(), x) == l and "<right>" or l
+      local y, x = unpack(api.nvim_win_get_cursor(0))
+      if in_string(x, y) then
+        return get(api.nvim_get_current_line(), x + 1) == l and "<right>" or l
       else
         return l .. l .. "<left>"
       end
