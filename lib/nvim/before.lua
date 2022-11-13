671970c5795082dfd350b2f1e0e0355952b0a390
figsoda <figsoda@pm.me>
2022-11-12 20:42:17 -0500

    nvim: override stylize_markdown with noice

--- lib/nvim/before.lua
+++ lib/nvim/before.lua
@@ -29,8 +29,9 @@ require("noice").setup({
 
   lsp = {
     override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["cmp.entry.get_documentation"] = true,
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
     },
   },
 
