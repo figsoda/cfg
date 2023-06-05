dc345e960043ee37545c3b64b36ca7f1afc3ec48
figsoda <figsoda@pm.me>
2023-06-05 14:19:44 -0400

    r: fix

--- src/_pkgs/r.nix
+++ src/_pkgs/r.nix
@@ -4,6 +4,6 @@ pkgs.writers.writeBashBin "r" ''
   if [ "$1" = cargo ] && nix eval --raw "nixpkgs#$1-$2" 2> /dev/null; then
     ${config.nix.package}/bin/nix shell "nixpkgs#$1-$2" -c cargo "''${@:2}"
   else
-    ${config.nix.package}/bin/nix run "nixpkgs#$pkg" -- "''${@:2}"
+    ${config.nix.package}/bin/nix run "nixpkgs#$1" -- "''${@:2}"
   fi
 ''
