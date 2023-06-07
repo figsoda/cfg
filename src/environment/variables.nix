6a4ca0ffc831f877f8adac5adfcda0d797df6206
figsoda <figsoda@pm.me>
2023-06-07 11:14:45 -0400

    simplify RIPGREP_CONFIG_PATH

--- src/environment/variables.nix
+++ src/environment/variables.nix
@@ -10,10 +10,10 @@ in
   CC = "gcc";
   LESSHISTFILE = "-";
   PATH = "$HOME/.cargo/bin";
-  RIPGREP_CONFIG_PATH = toString (writeText "rg-config" ''
+  RIPGREP_CONFIG_PATH = writeText "rg-config" ''
     -S
     -g=!.git
     --hidden
-  '');
+  '';
   fish_features = "qmark-noglob";
 }
