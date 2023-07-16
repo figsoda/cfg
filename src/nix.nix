1e36caa3aeaabb89a27b51276dc56f91a7d32d99
figsoda <figsoda@pm.me>
2023-07-16 12:17:20 -0400

    nix: add wheel to trusted users

--- src/nix.nix
+++ src/nix.nix
@@ -43,5 +43,6 @@
       "figsoda.cachix.org-1:mJfTEL4qLCqymqynJlaTxxi5APlaM0DfWg+h+CRGa20="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    trusted-users = [ "root" "@wheel" ];
   };
 }
