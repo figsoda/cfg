b1c0883a1018bddddba4aa36487fa9aec67e32fa
figsoda <figsoda@pm.me>
2023-08-15 21:25:22 -0400

    gtk4: configure libadwaita color scheme

--- src/environment/etc.nix
+++ src/environment/etc.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, root }:
 
 let
-  inherit (lib) generators;
+  inherit (lib) concatStrings generators mapAttrsToList;
   inherit (pkgs) fish formats libsecret substituteAll;
 
   gtkSettings = generators.toINI { } {
@@ -54,6 +54,54 @@ in
   };
 
   "xdg/gtk-3.0/settings.ini".text = gtkSettings;
+
+  "xdg/gtk-4.0/gtk.css".text = concatStrings (mapAttrsToList (k: v: "@define-color ${k} ${v};\n") (with root.colors; {
+    accent_bg_color = blue;
+    accent_color = blue;
+    accent_fg_color = black;
+    card_bg_color = black;
+    card_fg_color = white;
+    destructive_bg_color = red;
+    destructive_color = red;
+    destructive_fg_color = black;
+    dialog_bg_color = black;
+    dialog_fg_color = white;
+    error_bg_color = red;
+    error_color = red;
+    error_fg_color = black;
+    headerbar_backdrop_color = black;
+    headerbar_bg_color = black;
+    headerbar_border_color = black;
+    headerbar_darker_shade_color = black;
+    headerbar_fg_color = white;
+    headerbar_shade_color = black;
+    popover_bg_color = black;
+    popover_fg_color = white;
+    popover_shade_color = white;
+    scrollbar_outline_color = black;
+    secondary_sidebar_backdrop_color = black;
+    secondary_sidebar_bg_color = black;
+    secondary_sidebar_fg_color = white;
+    secondary_sidebar_shade_color = black;
+    shade_color = black;
+    sidebar_backdrop_color = black;
+    sidebar_bg_color = black;
+    sidebar_fg_color = white;
+    sidebar_shade_color = black;
+    success_bg_color = green;
+    success_color = green;
+    success_fg_color = black;
+    thumbnail_bg_color = black;
+    thumbnail_fg_color = white;
+    view_bg_color = black;
+    view_fg_color = white;
+    warning_bg_color = yellow;
+    warning_color = yellow;
+    warning_fg_color = black;
+    window_bg_color = black;
+    window_fg_color = white;
+  }));
+
   "xdg/gtk-4.0/settings.ini".text = gtkSettings;
 
   "xdg/nix-init/config.toml".source = (formats.toml { }).generate "config.toml" {
