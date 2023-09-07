634471166cac34f7066b54fdbdef44d4772fc094
figsoda <figsoda@pm.me>
2023-09-07 10:14:51 -0400

    exa -> eza

--- src/programs/fish/shellAliases.nix
+++ src/programs/fish/shellAliases.nix
@@ -1,12 +1,12 @@
 { pkgs }:
 
 let
-  inherit (pkgs) coreutils exa;
+  inherit (pkgs) coreutils eza;
 in
 
 {
   cp = "${coreutils}/bin/cp -ir";
-  ls = "${exa}/bin/exa -bl --git --icons --time-style long-iso --group-directories-first";
+  ls = "${eza}/bin/eza -bl --git --icons --time-style long-iso --group-directories-first";
   mv = "${coreutils}/bin/mv -i";
   rm = "${coreutils}/bin/rm -I";
 }
