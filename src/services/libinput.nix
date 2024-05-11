696d482b9b8e92b7a783cb43de0340ab579ff4a9
figsoda <figsoda@pm.me>
2024-05-11 14:56:24 -0400

    libinput: follow rename

---
+++ src/services/libinput.nix
@@ -0,0 +1,12 @@
+{
+  enable = true;
+  mouse = {
+    accelProfile = "flat";
+    accelSpeed = "0";
+    middleEmulation = false;
+  };
+  touchpad = {
+    accelProfile = "flat";
+    accelSpeed = "0";
+  };
+}
--- src/services/xserver.nix
+++ src/services/xserver.nix
@@ -7,18 +7,6 @@ in
 {
   enable = true;
   displayManager.sx.enable = true;
-  libinput = {
-    enable = true;
-    mouse = {
-      accelProfile = "flat";
-      accelSpeed = "0";
-      middleEmulation = false;
-    };
-    touchpad = {
-      accelProfile = "flat";
-      accelSpeed = "0";
-    };
-  };
   windowManager.awesome = {
     enable = true;
     noArgb = true;
