4daa994854260d7b7711bc1c06d2328374e9013d
figsoda <figsoda@pm.me>
2024-09-16 11:40:51 -0400

    i18n: follow renames

--- src/i18n.nix
+++ src/i18n.nix
@@ -2,8 +2,9 @@
 
 {
   inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
     fcitx5.addons = [ pkgs.fcitx5-rime ];
+    type = "fcitx5";
   };
   supportedLocales = [ "en_US.UTF-8/UTF-8" ];
 }
