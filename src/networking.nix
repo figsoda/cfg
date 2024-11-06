24482f680f72f0bc720670af54fa1856a8f0be9d
figsoda <figsoda@pm.me>
2024-11-05 22:20:25 -0500

    dns: add quad9 ipv6

--- src/networking.nix
+++ src/networking.nix
@@ -1,5 +1,10 @@
 {
-  nameservers = [ "9.9.9.9" "149.112.112.122" ];
+  nameservers = [
+    "2620:fe:11"
+    "2620:fe::fe:11"
+    "9.9.9.9"
+    "149.112.112.122"
+  ];
   networkmanager = {
     enable = true;
     dns = "none";
