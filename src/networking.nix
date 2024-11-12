97d12e46dcacd6023c321fd98e9c3f3da0c8664a
figsoda <figsoda@pm.me>
2024-11-12 16:45:13 -0500

    networking: let dhcpcd handle ethernet

--- src/networking.nix
+++ src/networking.nix
@@ -1,4 +1,6 @@
 {
+  dhcpcd.wait = "background";
+  interfaces.enp8s0f3u1c2.useDHCP = true;
   nameservers = [
     "2620:fe:11"
     "2620:fe::fe:11"
@@ -9,6 +11,7 @@
     enable = true;
     dns = "none";
     ethernet.macAddress = "random";
+    unmanaged = [ "type:ethernet" ];
     wifi.macAddress = "random";
   };
   useDHCP = false;
