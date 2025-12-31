25aaf9435f743171cd8594dfaab66e9183347cca
figsoda <figsoda@pm.me>
2025-12-31 14:51:13 -0500

    ssh: update darwin-build-box public key

--- src/programs/ssh/knownHosts.nix
+++ src/programs/ssh/knownHosts.nix
@@ -2,5 +2,5 @@
   "aarch64.nixos.community".publicKey =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds";
   "darwin-build-box.nix-community.org".publicKey =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz8FXSVEdf8FvDMfboxhB5VjSe7y2WgSa09q1L4t099";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMHhlcn7fUpUuiOFeIhDqBzBNFsbNqq+NpzuGX3e6zv";
 }
